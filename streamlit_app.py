01f5ebf3c7bfedb9ac59ef595d424f750172c42c
jcamminga <154283331+jcamminga@users.noreply.github.com>
2023-12-20 15:03:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,78 +1,66 @@
 import streamlit as st
 import pandas as pd
-import random
 
-# Streamlit-applicatie
 def get_random_question(excel_file_path, question_column, answer_column):
-    # Laad het Excel-bestand met de vragen en antwoorden
     df = pd.read_excel(excel_file_path)
-
-    # Selecteer een willekeurige rij
     random_row = df.sample(1)
-
-    # Haal de vraag en het antwoord op uit de geselecteerde rij
     question = random_row[question_column].values[0]
     answer = random_row[answer_column].values[0]
-
     return question, answer
 
+def set_question_and_answer(category):
+    if category == 'Vraag 1':
+        return get_random_question('champion-title.xlsx', 'champ-list__item__title', 'champ-list__item__name')
+    elif category == 'Vraag 2':
+        return get_random_question('champion-abilities.xlsx', 'ability-list__item__name', 'combined')
+    # Voeg hier meer categorieën toe als dat nodig is
+
+def show_question_page():
+    st.header(st.session_state.current_page)
+    st.write('Vraag: ' + st.session_state.question)
+
+    if st.button('Toon antwoord'):
+        st.write('Antwoord: ' + st.session_state.answer)
+        st.session_state.show_answer = True
+
+    if st.button('Terug'):
+        st.session_state.question, st.session_state.answer = None, None
+        st.session_state.show_answer = False
+        st.session_state.current_page = 'Home'
 
 def main():
     st.title('Vragen en Antwoorden App')
 
-    # Initialisatie van st.session_state
     if 'question' not in st.session_state:
         st.session_state.question = None
     if 'answer' not in st.session_state:
         st.session_state.answer = None
     if 'show_answer' not in st.session_state:
         st.session_state.show_answer = False
+    if 'current_page' not in st.session_state:
+        st.session_state.current_page = 'Home'
 
-    # Overzichtspagina met knoppen
-    page = st.sidebar.selectbox('Selecteer een pagina:', ['Home', 'Vraag 1', 'Vraag 2', 'Vraag 3', 'Vraag 4', 'Vraag 5', 'Vraag 6'])
+    page_options = ['Home', 'Vraag 1', 'Vraag 2']  # Voeg hier meer categorieën toe
+    page = st.sidebar.selectbox('Selecteer een pagina:', page_options)
 
     if page == 'Home':
         st.header('Welkom op de overzichtspagina!')
         st.write('Kies een vraagcategorie hieronder:')
-
-        # Knoppen voor verschillende vraagcategorieën
-        if st.button('Vraag 1'):
-            st.session_state.question, st.session_state.answer = get_random_question('champion-title.xlsx', 'champ-list__item__title', 'champ-list__item__name')
-            st.session_state.show_answer = False
-            page = 'Vraag 1'  # Schakel automatisch naar de Vraag 1-pagina
-
-        if st.button('Vraag 2'):
-            st.session_state.question, st.session_state.answer = get_random_question('champion-abilities.xlsx', 'ability-list__item__name', 'combined')
-            st.session_state.show_answer = False
-            page = 'Vraag 2'  # Schakel automatisch naar de Vraag 2-pagina
-
-        # Voeg hier de knoppen voor Vraag 3 tot Vraag 6 toe op dezelfde manier
+        for category in page_options[1:]:
+            if st.button(category):
+                st.session_state.question, st.session_state.answer = set_question_and_answer(category)
+                st.session_state.show_answer = False
+                st.session_state.current_page = category
 
     elif page.startswith('Vraag'):
-        st.header(page)
-        st.write('Vraag: ' + st.session_state.question)
-
-        # Knop om antwoord te tonen
-        if st.button('Toon antwoord'):
-            st.write('Antwoord: ' + st.session_state.answer)
-            st.session_state.show_answer = True
-
-        # Knop om terug te gaan naar overzichtspagina
-        if st.button('Terug'):
-            st.session_state.question, st.session_state.answer = None, None
-            st.session_state.show_answer = False
-            page = 'Home'  # Schakel automatisch naar de Home-pagina
+        show_question_page()
 
     if st.session_state.show_answer and page.startswith('Vraag'):
         st.write('Antwoord: ' + st.session_state.answer)
 
     if page != 'Home':
-        # Laat een knop zien om opnieuw de Vraag-pagina te openen voor nieuwe vragen
         if st.button('Opnieuw Vraag'):
-            if page == 'Vraag 1':
-                st.session_state.question, st.session_state.answer = get_random_question('champion-title.xlsx', 'champ-list__item__title', 'champ-list__item__name')
-            elif page == 'Vraag 2':
-                st.session_state.question, st.session_state.answer = get_random_question('champion-abilities.xlsx', 'ability-list__item__name', 'combined')
+            st.session_state.question, st.session_state.answer = set_question_and_answer(st.session_state.current_page)
             st.session_state.show_answer = False
 
 if __name__ == '__main__':
