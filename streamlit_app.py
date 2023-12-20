32dab708af8839e1e7f89d2e5c4a5ac7d04c3e08
jcamminga <154283331+jcamminga@users.noreply.github.com>
2023-12-20 15:06:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,9 +46,13 @@ def main():
     if page == 'Home':
         st.header('Welkom op de overzichtspagina!')
         st.write('Kies een vraagcategorie hieronder:')
-        for category in page_options[1:]:
-            if st.button(category, on_click=set_question_and_answer, args=(category,)):
-                st.session_state.current_page = category
+        
+        with st.form(key='my_form'):
+            for category in page_options[1:]:
+                if st.form_submit_button(category):
+                    st.session_state.current_page = category
+                    st.session_state.question, st.session_state.answer = set_question_and_answer(category)
+                    st.session_state.show_answer = False
 
     elif page.startswith('Vraag'):
         show_question_page()
