5475575a8a560648676bb4ac5787ec178bd239d2
jcamminga <154283331+jcamminga@users.noreply.github.com>
2023-12-20 16:44:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if page == "Champion titles":
         st.write(st.session_state['answer'])
 
     if st.button("Nieuwe vraag"):
-        st.session_state['question'] = ""
+        st.session_state['question'], st.session_state['answer'] = select_random_question(df_titles, 'champ-list__item__title', 'champ-list__item__name')
 
 # Pagina: Champion passives
 elif page == "Champion passives":
