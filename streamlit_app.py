c49fd78a235ceb247e14299ef495f963c2497b1d
jcamminga <154283331+jcamminga@users.noreply.github.com>
2023-12-20 16:15:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,22 @@ if 'current_page' not in st.session_state:
     st.session_state['question'] = ""
     st.session_state['answer'] = ""
     st.session_state['question_counter'] = 0
+    st.session_state['last_question_counter'] = -1
 
 # Check voor paginawissel of nieuwe vraag
-if page != st.session_state['current_page'] or st.session_state['question_counter'] != st.session_state.get('last_question_counter', 0):
+if page != st.session_state['current_page'] or st.session_state['question_counter'] != st.session_state['last_question_counter']:
     st.session_state['current_page'] = page
     st.session_state['last_question_counter'] = st.session_state['question_counter']
 
     if page == "Champion titles":
         df = load_data("champion-title.xlsx")
+        question_column, answer_column = 'champ-list__item__title', 'champ-list__item__name'
     elif page == "Champion passives":
         df = load_data("champion-abilities.xlsx")
         df = df[df['ability-list__item__keybind'] == 'Passive']
-    st.session_state['question'], st.session_state['answer'] = select_random_question(df, 'champ-list__item__title', 'champ-list__item__name')
+        question_column, answer_column = 'ability-list__item__name', 'combined'
+
+    st.session_state['question'], st.session_state['answer'] = select_random_question(df, question_column, answer_column)
 
 # Pagina inhoud
 st.subheader(f"{page} Vraag")
