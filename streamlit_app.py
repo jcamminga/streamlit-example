ac294b8d7d88c6eaa394ff4f0665c5240990d070
jcamminga <154283331+jcamminga@users.noreply.github.com>
2023-12-21 16:09:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ elif selected_page == "Champion abilities":
     df_abilities_filtered = df_abilities[df_abilities['ability-list__item__keybind'] != 'Passive']
 
     if st.session_state.get('load_new_question', False):
-        st.session_state['question'], st.session_state['answer'] = select_random_question(df_abilities, 'full image', 'combined')
+        st.session_state['question'], st.session_state['answer'] = select_random_question(df_abilities_filtered, 'full image', 'combined')
         st.session_state['load_new_question'] = False  # Reset de vlag na het laden van de nieuwe vraag
 
     st.subheader("Welke ability is dit?:")
